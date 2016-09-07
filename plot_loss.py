152765426bfdd6e6938c4b80f1fea3f76f3250c8
Hrayr Harutyunyan <harhro@gmail.com>
2016-09-07 20:21:51 +0400

    validation loss plotting added

--- plot_loss.py
+++ plot_loss.py
@@ -29,11 +29,17 @@ def is_float(s):
 
 log = codecs.open(args.log).readlines()
 losses = [float(line.split()[-7]) for line in log if len(line.split()) >= 7 and is_float(line.split()[-7]) ]
+val_losses = [float(line.split()[-1]) for line in log if line.split()[0] == 'validation' ]
 losses = smoothen(losses, args.window)
-X = [float(line.split()[1]) for line in log if len(line.split()) >= 2 and is_float(line.split()[1]) ][-len(losses):]
-
+X = [float(line.split()[1]) for line in log if len(line.split()) >= 2 and
+        is_float(line.split()[1]) ][-len(losses):] # what the fuck :/ 
 if args.ymax > 0:
 	plt.ylim(0, args.ymax)
-	
+
+val_step = len(X) / len(val_losses)
+if val_step * len(val_losses) >= len(X):
+    val_step -= 1
+val_x = [X[i] for i in range(val_step,len(X), val_step)]
+plt.plot(val_x, val_losses)
 plt.plot(X, losses)
 plt.savefig(args.log + '.png')
